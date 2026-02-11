1ca854e0e6f21066d9e799876c021b126575a8fa
Semen <semen.ruman@gmail.com>
2026-02-11 16:30:35 +0300

    Добавил маршрутизацию и исправил ошибку неправильной загрузки _Layout.cshtml

--- MyLibraryMvcApp/Areas/Library/Controllers/BookController.cs
+++ MyLibraryMvcApp/Areas/Library/Controllers/BookController.cs
@@ -5,11 +5,14 @@ namespace MyLibraryMvcApp.Areas.Library.Controllers
     [Area("Library")]
     public class BookController : Controller
     {
+        [Route("{area}/Books")]
+        [Route("{area}")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("{area}/Add")]
         public IActionResult Add()
         {
             return View();
